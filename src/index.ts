83a4bf0838f81b6d601d75e3e0b3f467e17a3620
k586 <mail@k586.jp>
2025-08-17 10:43:28 +0900

    route 分けのテスト

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,34 @@
-import { Hono } from 'hono';
-const app = new Hono();
+import { Hono, Context } from 'hono';
 
-app.get('/', honoText);
+function main() {
 
-export default app;
+    const app = new Hono();
+    const arrayLength = 3;
+    const array = Array.from({ length: arrayLength }, function() { return new Hono(); });
+
+    array[0].get('/', articleHtml);
+    array[0].get('/:id', articleHtml);
+    array[1].get('/:id', editArticleHtml);
+    array[2].get('/', listArticleHtml);
+
+    app.route('/', array[0]);
+    app.route('/edit', array[1]);
+    app.route('/list', array[2]);
+
+    return app;
+
+}
+export default main();
 
 
-function honoText(context) {
-    return context.text('🔥');
+function articleHtml(context: Context) {
+    return context.text('/' + context.req.param('id'));
+}
+
+function editArticleHtml(context: Context) {
+    return context.text('/edit/' + context.req.param('id'));
+}
+
+function listArticleHtml(context: Context) {
+    return context.text('🔥🔥🔥');
 }
