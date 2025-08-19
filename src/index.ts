b3e9d9ba1bbb67068dcda16709e04fab3d464fcf
k586 <mail@k586.jp>
2025-08-19 20:17:23 +0900

    記述整理

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,14 @@ import { indexHtml, listHtml, articleHtml, editHtml } from './html-template';
 function main() {
 
     const app = new Hono();
-    const arrayLength = 1;
-    const array = Array.from({ length: arrayLength }, function() { return new Hono(); });
+    const page = new Hono();
 
-    array[0].get('/', indexHtml);
-    array[0].get('/list', listHtml);
-    array[0].get('/:id', articleHtml);
-    array[0].get('/:id/edit', editHtml);
+    page.get('/', indexHtml);
+    page.get('/list', listHtml);
+    page.get('/:id', articleHtml);
+    page.get('/:id/edit', editHtml);
 
-    app.route('/', array[0]);
+    app.route('/', page);
 
     return app;
 
